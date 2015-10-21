950c4667decda6981d2c9ea0b4e3aa075bdd4f8f
xtxo014 <toheeb.okenla@grainger.com>
2015-10-20 22:21:03 -0500

    clean up

--- Prayer Partners/index.js
+++ Prayer Partners/index.js
@@ -4,23 +4,8 @@ var boys = members.boys;
 var girls = members.girls;
 var everybody = members.allMembers;
 
-// logic.shuffle(boys);
-// logic.shuffle(girls);
+
 logic.shuffle(everybody);
-
-// logic.prayerPartnersBoys(boys, 11);
-// console.log("---");
-// logic.prayerPartnersGirls(girls, 12);
-
 // console.log("---");
 logic.prayerPartnersEverybody(everybody, 23) //23 members
 
-//reminder - made special case because of odd numbers
-/* So I'll only loop through up to my array length - 1
-	I'll store the person who is at the last position
-	Then I or paul, and naomi will just meet up with that
-	extra person :)
-
-	Which is why the shuffle is important. Don't want the 
-	same person to always be the extra one out.
-*/
--- Prayer Partners/logic.js
+++ Prayer Partners/logic.js
@@ -1,60 +1,8 @@
 var members = require("./members");
-var boys = members.boys;
-var girls = members.girls;
-var everybody = members.allMembers;//
+// var boys = members.boys;
+// var girls = members.girls;
+var everybody = members.allMembers;
 
-
-// //check to make sure a servant or rina is not extra one out
-//  var extraBoyOut = boys[boys.length-1]; //even boys now
-// var extraGirlOut = girls[girls.length-1];
-var extraPersonOut = everybody[everybody.length-1];//
-// console.log(extraBoyOut.name);
-// while(extraGirlOut.name == "Rina" || extraGirlOut.name == "Naomi") {
-// 	shuffle(girls);
-// }
-
-// while(extraPersonOut.name == "Rina" || extraPersonOut.name == 
-// 	"Naomi" || extraPersonOut.name == "Toheeb" || extraPersonOut.name == "Paul") {
-// 		shuffle(everybody);
-// }
-// function prayerPartnersBoys(array, max) {
-// 	var rand = getRandomNumberBoys(max);
-// 	for (var i = 0; i < array.length-1; i++) { //change "array.length-1 if new member"
-// 		var me = array[i];
-// 		// if i'm not paired
-// 		if(!me.paired) { 
-// 			//rand must not be my index or the index of one already paired
-// 			while(array[rand].paired == true || rand == i || me.checkMeetups(array[rand])) {   
-// 				rand = getRandomNumberBoys(max);
-// 			}
-// 			// i got a number of available person
-// 			var partner = array[rand];
-// 			partner.paired = true;
-// 			array[i].paired = true;
-// 			console.log(array[i].name + " " + partner.name);
-// 		}
-// 	}//forloop
-// }
-
-// function prayerPartnersGirls(array, max) {
-// 	console.log("Naomi/Toheeb will meet with " + array[array.length-1].name);
-// 	var rand = getRandomNumberGirls(max);
-// 	for (var i = 0; i < array.length; i++) { //change "array.length-1 if new member"
-// 		var me = array[i];
-// 		// if i'm not paired
-// 		if(array[i].paired == false) { 
-// 			//rand must not be my index or the index of one already paired
-// 			while(array[rand].paired == true || rand == i || me.checkMeetups(array[rand])) {   
-// 				rand = getRandomNumberGirls(max);
-// 			}
-// 			// i got a number of available person
-// 			var partner = array[rand];
-// 			partner.paired = true;
-// 			array[i].paired = true;
-// 			console.log(array[i].name + " " + partner.name);
-// 		}
-// 	}//forloop
-// }
 
 function prayerPartnersEverybody(array, max) {
 	shuffle(everybody);
@@ -65,7 +13,7 @@ function prayerPartnersEverybody(array, max) {
 		var rand = getRandomNumberEverybody(max);
 		if(me.paired == false) { 
 			//rand must not be my index or the index of one already paired
-			while(rand == i || array[rand].paired == true || me.checkMeetups(array[rand])) {   
+			while(rand == i || array[rand].paired || me.checkMeetups(array[rand])) {   
 				rand = getRandomNumberEverybody(max);
 				console.log(rand);
 			}
@@ -75,19 +23,8 @@ function prayerPartnersEverybody(array, max) {
 			array[i].paired = true;
 			console.log(array[i].name + " " + partner.name);
 		}
-	}//forloop
+	}
 }
-
-
-// function getRandomNumberBoys(max) { //odd number of boys, update if change
-// 	var rand = Math.floor( Math.random() * ((max) - 0) + 0 ); 
-// 	return rand;
-// }
-
-// function getRandomNumberGirls(max) { //odd number of girls, update if change
-// 	var rand = Math.floor( Math.random() * ((max + 1) - 0) + 0 ); 
-// 	return rand;
-// }//remember the trouble math.floor caused you
 
 function getRandomNumberEverybody(max) { //odd number of members, update if change
 	var rand = Math.floor(Math.random() * (max-1) + 1 ); 
@@ -113,9 +50,5 @@ function shuffle(array) {
   return array;
 }
 
-// exports.prayerPartnersBoys = prayerPartnersBoys;
-// exports.prayerPartnersGirls = prayerPartnersGirls;
-// exports.getRandomNumberBoys = getRandomNumberBoys;
-// exports.getRandomNumberGirls = getRandomNumberGirls;
 exports.prayerPartnersEverybody = prayerPartnersEverybody;
 exports.shuffle = shuffle;
